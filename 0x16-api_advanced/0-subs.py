9e609d303cf201095cfc20df6404f2f41925450b
kmangaty <kmaangaty@gmail.com>
2024-03-15 18:49:12 +0200

    data set 3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-   Queries the Reddit API and returns the number of subscribers for a given subreddit.
+   Queries the Reddit API and returns the number
+    of subscribers for a given subreddit.
 
    Args:
        subreddit (str): The name of the subreddit to query.
@@ -14,33 +15,31 @@ import requests
 
 def number_of_subscribers(subreddit):
     """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    Queries the Reddit API and returns the number
+     of subscribers for a given subreddit.
 
     Args:
         subreddit (str): The name of the subreddit to query.
 
     Returns:
-        int: The number of subscribers of the subreddit. Returns 0 if the subreddit is invalid
+        int: The number of subscribers of the subreddit
+        . Returns 0 if the subreddit is invalid
         or if there was an error querying the API.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User Agent'}  # Set a custom User-Agent
+    headers = {'User-Agent': 'Custom User Agent'}
     response = requests.get(url, headers=headers)
 
-    # Check if the response is successful (status code 200)
     if response.status_code == 200:
-        # Parse the JSON response
         data = response.json()
-        # Extract and return the number of subscribers
         return data['data']['subscribers']
-    elif response.status_code == 404:  # Subreddit not found
+    elif response.status_code == 404:
         return 0
     else:
         print(f"Error: {response.status_code}")
         return 0
 
 
-# Test the function
 if __name__ == "__main__":
     subreddit = input("Enter subreddit name: ")
     print(number_of_subscribers(subreddit))
