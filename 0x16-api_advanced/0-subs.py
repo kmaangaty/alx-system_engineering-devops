7854479dd6648df5c1d914f015e4fab3e800ad2d
kmangaty <kmaangaty@gmail.com>
2024-03-15 19:24:15 +0200

    data set 3

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,45 +1,16 @@
 #!/usr/bin/python3
-"""
-   Queries the Reddit API and returns the number
-    of subscribers for a given subreddit.
-
-   Args:
-       subreddit (str): The name of the subreddit to query.
-
-   Returns:
-       int: The number of subscribers of the subreddit. Returns 0 if the subreddit is invalid
-       or if there was an error querying the API.
-"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number
-     of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit to query.
-
-    Returns:
-        int: The number of subscribers of the subreddit
-        . Returns 0 if the subreddit is invalid
-        or if there was an error querying the API.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User Agent'}
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    elif response.status_code == 404:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    else:
-        print(f"Error: {response.status_code}")
-        return 0
-
-
-if __name__ == "__main__":
-    subreddit = input("Enter subreddit name: ")
-    print(number_of_subscribers(subreddit))
+    results = response.json().get("data")
+    return results.get("subscribers")
