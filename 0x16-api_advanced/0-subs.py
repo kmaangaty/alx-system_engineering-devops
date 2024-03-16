f590d36e310423f2df8419f1cbd14c77a15c9a22
kmangaty <kmaangaty@gmail.com>
2024-03-17 00:57:36 +0200

    data set 6

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+"""
+    recursive function that queries the Reddit API and
+    returns a list containing the titles of all hot articles
+    for a given subreddit. If no results are found
+    for the given subreddit, the function should return None.
+"""
 import requests
 
 
