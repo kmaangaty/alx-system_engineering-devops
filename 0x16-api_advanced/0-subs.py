a935b3b4db608408e2ad23a41a486ae9927c447e
kmangaty <kmaangaty@gmail.com>
2024-03-17 01:16:18 +0200

    data set 6

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/programming/about.json"
-    headers = {'User-Agent': 'Mozilla/10.0/API'}
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        'User-Agent': 'Mozilla/10.0/API'
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
