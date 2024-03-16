e4dbefd3c59c07244c1318995e4f569216a9de10
kmangaty <kmaangaty@gmail.com>
2024-03-17 01:08:53 +0200

    data set 6

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,30 +9,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number
-     of subscribers for a given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The total number of subscribers for the subreddit.
-        Returns None if the subreddit is not found or if there is an error.
-    """
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/10.0/API'}
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the response is successful (status code 200)
-    if response.status_code == 200:
-        data = response.json().get("data")
-        return data.get('subscribers')
-    elif response.status_code == 404:
-        print("Error: {}".format(response.status_code))
-    elif response.status_code == 403:
-        print("Error: {}".format(response.status_code))
+    if response.status_code == 404:
         return 0
-    else:
-        print("Error: {}".format(response.status_code))
-        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
