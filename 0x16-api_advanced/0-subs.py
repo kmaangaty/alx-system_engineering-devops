de839de47eaba65a76b46fe4619d015070634ce5
kmangaty <kmaangaty@gmail.com>
2024-03-17 01:20:45 +0200

    data set 5

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,21 +9,18 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """
+        recursive function that queries the Reddit API and
+        returns a list containing the titles of all hot articles
+        for a given subreddit. If no results are found
+        for the given subreddit, the function should return None.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        'User-Agent': 'Mozilla/10.0/API'
-    }
+    headers = {"User-Agent": "My user Agent 1.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    elif response.status_code == 200:
-        try:
-            results = response.json().get("data")
-            return results.get("subscribers")
-        except Exception as e:
-            print("Error decoding JSON response:", e)
-            print("Response content:", response.content)
-            return None
+    if response.status_code == 200:
+        data = response.json().get("data", {})
+        sub_count = data.get("subscribers", 0)
+        return sub_count
     else:
-        return response.status_code
+        return 0
